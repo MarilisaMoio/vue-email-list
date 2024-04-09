d97cfe3d17a703a665bab76dc04d40667866adb6
Marilisa Moio <moiomarilisa@gmail.com>
2024-04-09 14:26:05 +0200

    feat: add basic mail generator

--- js/script.js
+++ js/script.js
@@ -3,8 +3,20 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            mailArray: [],
         };
     },
     methods: {
+        mailGenerator(){
+            for (let i = 1; i <= 10; i++){
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((response) => {
+                this.mailArray.push(response.data.response)
+                });
+            };
+        },
     },
+    mounted() {
+        this.mailGenerator()
+    }
 }).mount('#app');
